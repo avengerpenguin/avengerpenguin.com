77ef8cf242ed5cd19a2d4f519edf0bd47bfc7f93
Ross Fenning <ross.fenning@bbc.co.uk>
2022-02-06 12:53:49 +0000

    Move extra dir to top so Obsidian cannot see it

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,9 +21,18 @@ TEMPLATE_PAGES = {
     'search.html': 'search/index.html',
 }
 
-STATIC_PATHS = ["extra/CNAME"]
+STATIC_PATHS = ["../extra"]
 EXTRA_PATH_METADATA = {
-    "extra/CNAME": {"path": "CNAME"},
+    "../extra/CNAME": {"path": "CNAME"},
+    "../extra/android-chrome-144x144.png": {"path": "android-chrome-144x144.png"},
+    "../extra/apple-touch-icon.png": {"path": "apple-touch-icon.png"},
+    "../extra/browserconfig.xml": {"path": "browserconfig.xml"},
+    "../extra/favicon-16x16.png": {"path": "favicon-16x16.png"},
+    "../extra/favicon-32x32.png": {"path": "favicon-32x32.png"},
+    "../extra/favicon.ico": {"path": "favicon.ico"},
+    "../extra/mstile-150x150.png": {"path": "mstile-150x150.png"},
+    "../extra/safari-pinned-tab.svg": {"path": "safari-pinned-tab.svg"},
+    "../extra/site.webmanifest": {"path": "site.webmanifest"},
 }
 
 
