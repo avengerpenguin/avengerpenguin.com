77bf0c864b2a178a78db4cee076d82f46f4f6443
Ross Fenning <ross.fenning@inrix.com>
2023-01-06 13:03:49 +0000

    Publish only the blips array as the radar tool wants only that now

--- radar.js
+++ radar.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const { createRadarJson } = require('tech-radar-markdown-tools');
+const path = require("path");
+const { createRadarJson } = require("tech-radar-markdown-tools");
 
 createRadarJson({
   title: "Ross Fenning's Tech Radar",
-  rings: ['Adopt', 'Trial', 'Assess', 'Hold'],
+  rings: ["Adopt", "Trial", "Assess", "Hold"],
   quadrants: [
-    path.resolve('./Garden/Tech Radar - Languages & Frameworks.md'),
-    path.resolve('./Garden/Tech Radar - Platforms.md'),
-    path.resolve('./Garden/Tech Radar - Techniques.md'),
-    path.resolve('./Garden/Tech Radar - Tools.md'),
+    path.resolve("./Garden/Tech Radar - Languages & Frameworks.md"),
+    path.resolve("./Garden/Tech Radar - Platforms.md"),
+    path.resolve("./Garden/Tech Radar - Techniques.md"),
+    path.resolve("./Garden/Tech Radar - Tools.md"),
   ],
-}).then(radar => {
-  console.log(JSON.stringify(radar));
+}).then((radar) => {
+  console.log(JSON.stringify(radar.blips));
 });
