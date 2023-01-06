3af076ebadb6f2fd16ee4ec060582fb4da2221ee
Ross Fenning <ross.fenning@inrix.com>
2023-01-06 12:50:52 +0000

    Use standard ring names

--- radar.js
+++ radar.js
@@ -2,8 +2,8 @@ const path = require('path');
 const { createRadarJson } = require('tech-radar-markdown-tools');
 
 createRadarJson({
-  title: 'My radar',
-  rings: ['Adopt', 'Explore', 'Endure', 'Retire'],
+  title: "Ross Fenning's Tech Radar",
+  rings: ['Adopt', 'Trial', 'Assess', 'Hold'],
   quadrants: [
     path.resolve('./Garden/Tech Radar - Languages & Frameworks.md'),
     path.resolve('./Garden/Tech Radar - Platforms.md'),
