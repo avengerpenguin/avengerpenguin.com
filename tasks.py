16aaa2781bee1759af6b6d42325a596390513d17
Ross Fenning <ross.fenning@inrix.com>
2023-01-10 15:12:56 +0000

    Create local radar at /radar so internal links work

--- tasks.py
+++ tasks.py
@@ -5,10 +5,21 @@ namespace = voltaire.site()
 
 
 @task
-def radar(c):
+def radar(c, output_path="output/radar"):
     c.run("node radar.js >extra/radar.json")
+    c.run(f"mkdir -p {output_path}")
+    c.run(
+        f"docker build -t radar -f Dockerfile.radar --output type=local,dest={output_path} ."
+    )
+
+
+@task
+def radar_publish(c):
+    radar(c, output_path="dest/radar")
 
 
 namespace.add_task(radar)
 namespace.task_with_config("build")[0].pre.append(radar)
-namespace.task_with_config("publish")[0].pre.append(radar)
+
+namespace.add_task(radar_publish)
+namespace.task_with_config("publish")[0].pre.append(radar_publish)
